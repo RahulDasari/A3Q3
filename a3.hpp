3a365e4dbe65e9ccce2f2d4b8d0a2c20924ce6ea
RahulDasari <rahul.dasari094@gmail.com>
2023-12-08 17:30:41 -0500

    cuda

--- a3.hpp
+++ a3.hpp
@@ -5,7 +5,7 @@
 #include "a3.cu"
 #ifndef A3_HPP
 #define A3_HPP
-#include "a3.cu"
+
 void gaussian_kde(int n, float h, const std::vector<float>& x, std::vector<float>& y) {
     gaussian_kernel( n, h, x, y);
 } // gaussian_kde
