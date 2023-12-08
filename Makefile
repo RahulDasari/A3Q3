cde717ba7476931f2b3448fc7faa04cb7a0d8b4b
RahulDasari <rahul.dasari094@gmail.com>
2023-12-08 14:59:09 -0500

    cuda

--- Makefile
+++ Makefile
@@ -1,19 +1,7 @@
-#CXX=g++
-#CXXFLAGS=-std=c++11 -O3
-
-#all: a3
-
-#clean:
-#	rm -rf a3
-CXX=nvcc
-CXXFLAGS=-std=c++17 -O3
+CXX=g++
+CXXFLAGS=-std=c++11 -O3
 
 all: a3
 
-a3: a3.o a3_host.o $(CXX) $(CXXFLAGS) a3_host.o a3.o -o a3
-
-a3.o: a3.cu a3.hpp $(CXX) $(CXXFLAGS) -c a3.cu -o a3.o
-
-a3_host.o: a3.cpp a3.hpp $(CXX) $(CXXFLAGS) -c a3.cpp -o a3_host.o
-
-clean: rm -rf a3
+clean:
+	rm -rf a3
--- a3.cu
+++ a3.cu
@@ -1,20 +1,17 @@
 #include"math.h"
-/* #include <cuda.h>
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h" */
 #include <chrono>
 #include <iostream>
 #include <random>
 #include <vector>
 #include <functional>
 
-//#include "a3.hpp"
 __global__ void compute_kernel(int n, float h, float* x, float* y){
     int i,j;
     i = blockIdx.x * blockDim.x + threadIdx.x;
     j = blockIdx.y * blockDim.y + threadIdx.y;
     if (i<n && j<n)
         y[j] += (1/(n*h))*(1/sqrt(2*M_PI)) * exp(-pow((x[i] - x[j])/h ,2 )/2);
+        printf("%d %d \n" y[j] ,(1/(n*h))*(1/sqrt(2*M_PI)) * exp(-pow((x[i] - x[j])/h ,2 )/2) );
 }
 
 void gaussian_kernel(int n, float h, const std::vector<float>& x, std::vector<float>& y) {
@@ -31,49 +28,3 @@ void gaussian_kernel(int n, float h, const std::vector<float>& x, std::vector<fl
     cudaFree(d_x);
     cudaFree(d_y);
 }
-void gaussian_kde(int n, float h, const std::vector<float>& x, std::vector<float>& y) {
-    gaussian_kernel(n,  h, x, y);
-} // gaussian_kde
-
-
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        std::cout << "usage: " << argv[0] << " n h" << std::endl;
-        return -1;
-    }
-
-    int n = std::atoi(argv[1]);
-    float h = std::atof(argv[2]);
-
-    if (n < 32) {
-        std::cout << "hey, n is too small even for debugging!" << std::endl;
-        return -1;
-    }
-
-    if (h < 0.00001) {
-        std::cout << "this bandwidth is too small" << std::endl;
-        return -1;
-    }
-
-    // in and out (in is set to 1s for fun)
-    std::vector<float> x(n);
-    std::vector<float> y(n, 0.0);
-
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::lognormal_distribution<float> N(0.0, 1.0);
-    std::generate(std::begin(x), std::end(x), std::bind(N, gen));
-
-    // now running your awesome code from a3.hpp
-    auto t0 = std::chrono::system_clock::now();
-
-    gaussian_kde(n, h, x, y);
-
-    auto t1 = std::chrono::system_clock::now();
-
-    auto elapsed_par = std::chrono::duration<double>(t1 - t0);
-    std::cout << elapsed_par.count() << std::endl;
-
-    return 0;
-}
-
