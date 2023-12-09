3b4d9ec5260f6632608432a0d341387f09a89b16
RahulDasari <rahul.dasari094@gmail.com>
2023-12-08 23:39:35 -0500

    make

--- Makefile
+++ Makefile
@@ -1,1 +1,13 @@
-nvcc a3.cu -o a3
+CXX=nvcc
+CXXFLAGS=-std=c++11
+
+all: a3
+
+a3: a3.o a3_host.o
+	$(CXX) $(CXXFLAGS) a3.o a3_host.o -o a3 -lcuda -lcudart
+	
+a3.o: a3.cu
+	$(CXX) $(CXXFLAGS) -c a3.cu -o a3.o
+	
+a3_host.o: a3.cpp
+	$(CXX) $(CXXFLAGS) -c a3.cpp -o a3_host.o
--- a3.hpp
+++ a3.hpp
@@ -5,43 +5,14 @@
 #ifndef A3_HPP
 #define A3_HPP
 
-
-#include <chrono>
-#include <iostream>
-#include <random>
-#include <vector>
-#include <functional>
-#include "math.h"
-#include "cuda.h"
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
+#include <cstdio>
+#include <cstdlib>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cmath>
+#include "a3.cuh"
 
 //#include "a3.hpp"
-
-__global__ void compute_kernel(int n, float h, float* x, float* y){
-    int i,j;
-    i = blockIdx.x * blockDim.x + threadIdx.x;
-    j = blockIdx.y * blockDim.y + threadIdx.y;
-    if (i<n && j<n){
-        y[j] += (1/(n*h))*(1/sqrt(2*M_PI)) * exp(-pow((x[i] - x[j])/h ,2 )/2);
-    }
-}
-
-void gaussian_kernel(int n, float h, const std::vector<float>& x, std::vector<float>& y) {
-    float * d_x ;
-    float * d_y;
-    cudaMalloc((void**)&d_x, sizeof(float) * n);
-    cudaMemcpy(d_x, x.data(), sizeof(float) * n, cudaMemcpyHostToDevice);
-    cudaMalloc((void**)&d_y, sizeof(float) * n);
-    cudaMemcpy(d_y, y.data(), sizeof(float) * n, cudaMemcpyHostToDevice);
-
-    dim3 num_threads = (1024,1024);
-    dim3 blocksize = ((n/1024) + 1,(n/1024)+1);
-    compute_kernel<<<blocksize , num_threads>>>(n ,h ,d_x, d_y);
-    cudaMemcpy(y.data(), d_y , sizeof(float) * n, cudaMemcpyHostToDevice);
-    cudaFree(d_x);
-    cudaFree(d_y);
-}
 
 void gaussian_kde(int n, float h, const std::vector<float>& x, std::vector<float>& y) {
     gaussian_kernel( n, h, x, y);
